6adca78192e68fb66b41dba1ffa6b64e6208b6b2
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-04-28 15:03:11 +0900

    土台を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_083240) do
+ActiveRecord::Schema.define(version: 2019_04_27_100159) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.boolean "done"
+    t.bigint "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_questions_on_category_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +42,6 @@ ActiveRecord::Schema.define(version: 2019_04_22_083240) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "categories"
+  add_foreign_key "questions", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: "人間関係・人生相談")
+Category.create(name: "趣味・娯楽・エンタメ")
+Category.create(name: "生活・暮らし")
+Category.create(name: "健康・病気")
+Category.create(name: "学問・教育")
+Category.create(name: "パソコン・スマホ")
+Category.create(name: "インターネット・WEBサービス")
+Category.create(name: "美容・ファッション")
+Category.create(name: "製造業・ファッション")
+Category.create(name: "ビジネス・キャリア")
+Category.create(name: "社会")
+Category.create(name: "大規模災害")
+Category.create(name: "お金")
+Category.create(name: "パソコン・スマホ")

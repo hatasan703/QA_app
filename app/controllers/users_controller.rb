2073431694f78d951eee1d0470b1cf3617663480
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-05-07 15:39:26 +0900

    ageカラムを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :sex, :bio)
+    params.require(:user).permit(:nickname, :sex, :bio, :age)
   end
 
 end

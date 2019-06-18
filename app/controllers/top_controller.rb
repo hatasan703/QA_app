ab26a0cf3c516c682f0e2de42be7e1efac028c61
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-06-18 20:56:37 +0900

    その他ページの作成とトップページの修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,10 @@
 class TopController < ApplicationController
 
   def index
+    @all_questions = Question.all
+    @questions = @all_questions.where(done: nil).limit(3)
+
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,25 +10,33 @@ Rails.application.routes.draw do
     get 'card/:id' => 'users#card'
     get 'bank/:id' => 'users#bank'
 
-
+  # 検索
   get 'questions/search' => 'questions#search'
   get 'questions/search_open' => 'questions#search_open'
   get 'questions/search_resolved' => 'questions#search_resolved'
 
+  # カテゴリ
   get 'questions/categories' => 'questions#categories'
   get 'questions/category/:id' => 'questions#category'
   get 'questions/category/:id/category_open' => 'questions#category_open'
 
+  # ランキング
   get 'questions/ranking' => 'questions#ranking'
   get 'questions/open' => 'questions#open'
   post 'questions/confirm' => 'questions#confirm'
 
-
+  # QA詳細
   resources :questions, only: :show do
     resources :answers, only: [:create, :update, :destroy]
     post 'confirm' => 'answers#confirm'
     get 'answers/:id/ba_confirm' => 'answers#ba_confirm'
   end
+
+  # フッター
+  get 'privacy_policy' => 'others#privacy_policy'
+  get 'rule' => 'others#rule'
+  get 'help' => 'others#help'
+  get 'contact' => 'others#contact'
 
 end
 

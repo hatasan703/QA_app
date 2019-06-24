a7a994f9b2552c90729e1dd3deb3861ca9ada62f
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-06-24 11:55:45 +0900

    トップページのカテゴリを修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,7 @@ class TopController < ApplicationController
   def index
     @all_questions = Question.all
     @questions = @all_questions.where(done: nil).limit(3)
-
-
+    @categories = Category.all
   end
 
 end

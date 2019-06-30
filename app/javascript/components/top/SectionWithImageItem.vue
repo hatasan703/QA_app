69811d12bc8bc9b281590c7419f5f1ff70186f83
konu96 <ukouhei96@gmail.com>
2019-06-30 16:27:58 +0900

    add SectionWithImageList and DiscussionButtonList

---
+++ app/javascript/components/top/SectionWithImageItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="howToItem">
+        <section><h3 v-html="title"></h3></section>
+        <img
+            :alt="imageAlt"
+            :src="imageSrc"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        imageSrc: {
+            type: String,
+            required: true,
+        },
+        imageAlt: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
---
+++ app/javascript/components/top/SectionWithImageList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="howToWrap">
+        <section-with-image-item
+            title="回答者への報酬額を決めて、相談する"
+            image-src="assets/howTo_item01.png"
+            image-alt=""
+        />
+
+        <section-with-image-item
+            title="集まった回答の中から<br>ベストアンサーを選ぶ"
+            image-src="assets/howTo_item02.png"
+            image-alt=""
+        />
+
+        <section-with-image-item
+            title="ベストアンサーに選ばれた回答者は<br>報酬が貰える"
+            image-src="assets/howTo_item03.png"
+            image-alt=""
+        />
+    </div>
+</template>
+
+<script>
+import SectionWithImageItem from './SectionWithImageItem.vue';
+import DiscussionButtonList from 'components/top/DiscussionButtonList.vue';
+
+export default {
+    components: {
+        SectionWithImageItem,
+        DiscussionButtonList,
+    },
+};
+
+</script>

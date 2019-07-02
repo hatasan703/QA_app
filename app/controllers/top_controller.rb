a36f0889a71f8fd4d1e194c3410ce7321c3a2793
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-07-02 14:58:40 +0900

    topページの修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,7 +5,7 @@ class TopController < ApplicationController
     @questions = @all_questions.where(done: nil).order('created_at DESC').limit(3)
     @categories = Category.all
 
-    @ranking_questions = Question.order('impressions_count DESC').limit(3)
+    @ranking_questions = Question.where(done: true).order('impressions_count DESC').limit(3)
   end
 
 end

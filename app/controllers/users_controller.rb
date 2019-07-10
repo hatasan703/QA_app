332171d4448ed713549bc661e8414d4fae33b316
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-07-10 18:43:58 +0900

    ユーザーに関する機能を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,9 @@ before_action :redirect_top, except: [:my_question, :my_answer]
 
   def edit
     @user = User.find(params[:id])
+    unless @user == current_user
+        redirect_to root_path
+    end
   end
 
   def update
@@ -44,21 +47,30 @@ before_action :redirect_top, except: [:my_question, :my_answer]
 
 
   def identification
+    @user = User.find(params[:id])
+    unless @user == current_user
+        redirect_to root_path
+    end
   end
 
   def card
-  end
-
-  def bank
+    @user = User.find(params[:id])
+    unless @user == current_user
+        redirect_to root_path
+    end
   end
 
   def point
+    @user = User.find(params[:id])
+    unless @user == current_user
+        redirect_to root_path
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :sex, :bio, :age, :role)
+    params.require(:user).permit(:user_name, :sex, :bio, :age, :role, :email)
   end
 
 end

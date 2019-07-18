4a5395b6a72c2064f3d5d0043be7ae23acfd83cf
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-07-18 18:07:48 +0900

    入力が空の場合は送信できないように設定

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,9 +3,6 @@ class Answer < ApplicationRecord
   belongs_to :question
   validates :text, presence: true
 
-
-
-
    # 自分の回答がBAに選定されたとき通知メソッド
    def ba_create_notification_by(current_user)
     notification = current_user.active_notifications.new(

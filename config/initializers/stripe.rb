1f7ffe20b4adf8e7ccc3bb110054fc420757663e
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-07-24 18:59:56 +0900

    stripeAPIキーを本番用に設定

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,22 @@
-Rails.configuration.stripe = {
-    :publishable_key => Rails.application.credentials.stripe[:publishable_key],#stipeのページに行って自分のアカウントから取ってくる
-    :secret_key      => Rails.application.credentials.stripe[:secret_key]
+# Rails.configuration.stripe = {
+#     :publishable_key => Rails.application.credentials.stripe[:publishable_key],#stipeのページに行って自分のアカウントから取ってくる
+#     :secret_key      => Rails.application.credentials.stripe[:secret_key]
+#   }
+
+#   Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+
+
+  Rails.configuration.stripe = {
+    # case Rails.env
+    # when 'development'
+    #     :publishable_key => Rails.application.credentials.stripe[:stripe_publishable_key_test],#stipeのページに行って自分のアカウントから取ってくる
+    #     :secret_key      => Rails.application.credentials.stripe[:stripe_secret_key_test]
+    # when 'production'
+        publishable_key: Rails.application.credentials.stripe[:stripe_publishable_key],#stipeのページに行って自分のアカウントから取ってくる
+        secret_key: Rails.application.credentials.stripe[:stripe_secret_key]
+    # end
+
   }
 
   Stripe.api_key = Rails.configuration.stripe[:secret_key]

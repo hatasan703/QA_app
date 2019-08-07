b831c805aec3a29cfa7995ef4cfe9e2d4c0fdc09
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-08-07 19:52:57 +0900

    運営通知のレイアウト,表示ロジックの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :search, except: [:search_resolved, :search_open]
-  before_action :unchecked_notifications
+  before_action :unchecked_notifications, :controller_action_logic
 
   def search
     @search = ::Question.ransack(params[:q])
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
     @questions = @search_questions.where(done: true).order('updated_at DESC').limit(10)
     @question_count = @search_questions.where(done: true).count
   end
-
 
   def redirect_top
     redirect_to controller: :top, action: :index unless user_signed_in?
@@ -20,7 +19,12 @@ class ApplicationController < ActionController::Base
      @notifications = current_user.passive_notifications.where(check: false).order("created_at DESC").limit(3)
     end
      @unchecked_articles = Article.where(check: false).order("created_at DESC")
+  end
 
+  def controller_action_logic
+    @question_new = params[:controller] == 'questions' && params[:action] == 'new'
+    @question_confirm = params[:controller] == 'questions' && params[:action] == 'confirm'
+    @top_index = params[:controller] == "top" && params[:action] == "index"
   end
 
 end

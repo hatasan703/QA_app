85b74c0f00e2f26b5853b9792d99de8126882a2d
hiroki hakeda <hirokihakeda@hirokinoMacBook-Pro.local>
2019-08-11 02:38:03 +0900

    weeklyランクのロジック一旦コミット

--- app/controllers/user_ranking_controller.rb
+++ app/controllers/user_ranking_controller.rb
@@ -1,15 +1,20 @@
 class UserRankingController < ApplicationController
 
     def week
-        @users = User.all
-        # @users.each do |user|
-        #     @best_answers = user.answers.where(best_answer: true)
-        #     @best_answers.each do |answer|
-        #         if answer.question.done_date ==７日以内
-        #             @my_point += answer.question.point
-        #         end
-        #     end
-        # end
+        questions = Question.where(done: true)
+        weekly_question = []
+        questions.each do |question|
+            if (Date.today.to_i - question.done_date) / 60 / 60 / 24 <= 7
+                weekly_question << question
+            end
+        end
+        user_point = {}
+        weekly_question.each do |question|
+            # user_id = question.user_id
+            # point = question.point
+            user_point.merge!(question.user_id => question.point)
+        end
+        binding.pry
     end
 
     def month

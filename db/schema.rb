5dcdc8d981f0fd27903ad720096c831c470e6eb3
kauto1716 <kauto1716@yahoo.co.jp>
2019-08-13 17:07:07 +0900

    こんにちは

--- db/schema.rb
+++ db/schema.rb
@@ -132,8 +132,8 @@ ActiveRecord::Schema.define(version: 2019_08_09_050722) do
     t.integer "money", default: 0
     t.text "bio"
     t.string "role"
-    t.text "image"
     t.integer "age"
+    t.text "image"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
